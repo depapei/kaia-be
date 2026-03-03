9898e55dd680412a64fa9e9060e0b423c8d02aeb
RangSheet <ranggaa606@gmail.com>
2026-03-03 16:49:35 +0700

    add wishlist index get data controller

--- controllers/wishlist/index.go
+++ controllers/wishlist/index.go
@@ -9,18 +9,57 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type SliceOpt struct {
+	Slices string  `json:"slices"`
+	Price  float64 `json:"price"`
+}
+
+type WishlistResponse struct {
+	ID              string     `json:"id"`
+	Name            string     `json:"name"`
+	Price           float64    `json:"price"`
+	Category        string     `json:"category"`
+	Description     string     `json:"desc"`
+	LongDescription string     `json:"longDesc"`
+	Image           string     `json:"image"`
+	SlicesOptions   []SliceOpt `json:"sliceOptions"`
+}
+
 func Index(c *gin.Context) {
 	var wishlists []model.Wishlist
-	var user_id = c.Query("user_id")
+	var user_id = c.Param("user_id")
 
 	if user_id == "" {
 		c.JSON(http.StatusBadRequest, res.Fail{
-			Message: "user_id cannot be empty",
+			Message: "User id tidak boleh kosong",
 		})
 		return
 	}
 
-	result := DataAccess.DB.Find(&wishlists).Where("user_id = ?", user_id).Limit(50)
+	result := DataAccess.DB.Preload("Product.ProductSlices").Find(&wishlists).Where("user_id = ?", user_id).Limit(50)
+
+	var response []WishlistResponse
+	for _, wishlist := range wishlists {
+
+		var sliceOpt []SliceOpt
+		for _, productSlice := range wishlist.Product.ProductSlices {
+			sliceOpt = append(sliceOpt, SliceOpt{
+				Slices: productSlice.Slice,
+				Price:  productSlice.Price,
+			})
+		}
+
+		response = append(response, WishlistResponse{
+			Name:            wishlist.Product.Name,
+			ID:              wishlist.ProductID,
+			Price:           float64(wishlist.Product.Price),
+			Description:     wishlist.Product.Description,
+			LongDescription: wishlist.Product.Longdesc,
+			Image:           wishlist.Product.Image,
+			Category:        wishlist.Product.Category,
+			SlicesOptions:   sliceOpt,
+		})
+	}
 
 	if result.Error != nil {
 		c.JSON(http.StatusInternalServerError, res.Fail{
@@ -31,6 +70,6 @@ func Index(c *gin.Context) {
 
 	c.JSON(http.StatusOK, res.Success{
 		Success: true,
-		Data:    wishlists,
+		Data:    response,
 	})
 }
